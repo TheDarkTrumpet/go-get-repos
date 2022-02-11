f14d092df812027904f7967f3809f2fd67af8f7b
David Thole <david@thedarktrumpet.com>
2022-02-10 20:36:41 -0600

    Moving util into printheaders and adding reference to main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/google/go-github/github"
+	"go-get-repos/util"
 	"golang.org/x/oauth2"
 	"io/ioutil"
 	"log"
@@ -44,7 +45,7 @@ func main() {
 	}
 	backupDirFiles := getBackupDirectoryNames(backupDirContents)
 	fmt.Printf("Current Directory Contents: \n")
-	PrintList(backupDirFiles)
+	util.PrintList(backupDirFiles)
 
 	// Get all repos from Github
 	var availableRepos []github.Repository
@@ -88,7 +89,7 @@ type GHVars struct {
 }
 
 func loadVars() (GHVars, error) {
-	PrintHeader(fmt.Sprintf("Loading Creds from %v", creds))
+	util.PrintHeader(fmt.Sprintf("Loading Creds from %v", creds))
 	var vars GHVars
 
 	_, err := os.Stat(*creds)
@@ -105,7 +106,7 @@ func loadVars() (GHVars, error) {
 }
 
 func readBackupDirectory(vars GHVars) ([]os.FileInfo, error) {
-	PrintHeader(fmt.Sprintf("Reading backup directory: %s", vars.BackupDir))
+	util.PrintHeader(fmt.Sprintf("Reading backup directory: %s", vars.BackupDir))
 	files, err := ioutil.ReadDir(vars.BackupDir)
 	return files, err
 }
@@ -130,7 +131,7 @@ func getGithubClient(vars GHVars) *github.Client {
 }
 
 func readPersonalGithubRepos(vars GHVars) ([]github.Repository, error) {
-	PrintHeader("Reading PERSONAL Github Repos")
+	util.PrintHeader("Reading PERSONAL Github Repos")
 	ghRepos := make([]github.Repository, 0, 20)
 	page := 1
 	for {
@@ -157,7 +158,7 @@ func readPersonalGithubRepos(vars GHVars) ([]github.Repository, error) {
 }
 
 func readOrganizationGithubRepos(vars GHVars) ([]github.Repository, error) {
-	PrintHeader("Reading ORG Github Repos")
+	util.PrintHeader("Reading ORG Github Repos")
 	ghRepos := make([]github.Repository, 0, 20)
 
 	for _, tpe := range vars.Types {
@@ -207,7 +208,7 @@ func getReposToClone(files []string, repos []github.Repository) []github.Reposit
 }
 
 func cloneNonBackedupRepos(repos []github.Repository, vars GHVars) (int, error) {
-	PrintHeader(fmt.Sprintf("Cloning all non-cached repos, number to process: %v", len(repos)))
+	util.PrintHeader(fmt.Sprintf("Cloning all non-cached repos, number to process: %v", len(repos)))
 	numReposProcessed := 0
 	err := error(nil)
 	for _, repo := range repos {
@@ -232,7 +233,7 @@ func updateAllCachedRepos(vars GHVars) (int, error) {
 		return numReposUpdated, err
 	}
 
-	PrintHeader(fmt.Sprintf("Updating all cached repos, number to process: %v", len(backupDirectoryFiles)))
+	util.PrintHeader(fmt.Sprintf("Updating all cached repos, number to process: %v", len(backupDirectoryFiles)))
 
 	for _, repo := range backupDirectoryFiles {
 		fmt.Printf("==> Processing: %v\n", repo.Name())
